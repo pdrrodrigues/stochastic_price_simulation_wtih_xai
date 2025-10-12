9394383c9f425cabd65f0b0bdc3ccd60fb2941e4
pedro <pedroromerorodrigues@outlook.com>
2025-10-12 10:19:13 +0000

    corrected gbm test

--- tests/test_gbm.py
+++ tests/test_gbm.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ..src.sim.gbm import gbm
+from sim.gbm import gbm
 
 
 def test_gbm_mean_variance():
@@ -10,18 +10,22 @@ def test_gbm_mean_variance():
     sigma = 0.2
     T = 1
     dt = 1 / 252
+    n_paths = 10000
 
     # Simulate
-    S = gbm(S0, mu, sigma, T, dt)
+    S = gbm(S0, mu, sigma, T, dt, n_paths)
 
     # Theoretical mean and variance
     mean = S0 * np.exp(mu * T)
     variance = S0**2 * np.exp(2 * mu * T) * (np.exp(sigma**2 * T) - 1)
 
+    # Final values
+    final_value = S.iloc[-1].values
+
     # Calculate the empirical mean and variance
-    empirical_mean = np.mean(S)
-    empirical_variance = np.var(S)
+    empirical_mean = np.mean(final_value)
+    empirical_variance = np.var(final_value)
 
     # Assert that the empirical mean and variance are close to the theoretical values
-    assert np.isclose(empirical_mean, mean, rtol=1e-2)
-    assert np.isclose(empirical_variance, variance, rtol=5e-2)
+    assert np.isclose(empirical_mean, mean, rtol=0.05)
+    assert np.isclose(empirical_variance, variance, rtol=0.1)
